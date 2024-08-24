3b5f89bdcc6dd01f33f74a789327b880cb9bdb81
Manuel Bogner <manuel.bogner@openresearch.com>
2024-08-25 01:42:22 +0200

    pull together kraft and kafka connect

--- kraft/ubuntu/build-kraft-ubuntu.sh
+++ kraft/ubuntu/build-kraft-ubuntu.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}" || exit 99
-source ../_shared.sh || exit 98
+source ../../_shared.sh || exit 98
 
 # latest version see https://downloads.apache.org/kafka/
 VERSION="ubuntu_2.13-3.8.0"
-../download.sh "https://downloads.apache.org/kafka/3.8.0/kafka_2.13-3.8.0.tgz" kafka.tgz || exit 97
+../../download.sh "https://downloads.apache.org/kafka/3.8.0/kafka_2.13-3.8.0.tgz" kafka.tgz || exit 97
 
-cp ../downloads/kafka.tgz . || exit 96
+cp ../../downloads/kafka.tgz . || exit 96
 build "kraft-ubuntu" "$DIR" "mbopm/kraft" "$VERSION" "" || exit 1
 rm -f kafka.tgz
