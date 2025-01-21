c342327bda286f90dbb22755dda8f172ee148e32
Manuel Bogner <manuel.bogner@openresearch.com>
2025-01-21 20:28:04 +0100

    create plugins folder if missing

--- sonarqube/custom-entrypoint.sh
+++ sonarqube/custom-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
-if [[ ! -f /opt/sonarqube/extensions/plugins/initialised ]]; then
-  echo "copy default plugins"
-  cp /opt/sonarqube/docker/plugins/*.jar /opt/sonarqube/extensions/plugins/
+PLUGINS=/opt/sonarqube/extensions/plugins
+
+if [[ ! -d $PLUGINS ]]; then
+  mkdir -p $PLUGINS
 fi
-touch /opt/sonarqube/extensions/plugins/initialised
+if [[ ! -f $PLUGINS/initialised ]]; then
+  echo "copy default plugins"
+
+  cp /opt/sonarqube/docker/plugins/*.jar $PLUGINS
+fi
+touch $PLUGINS/initialised
 /opt/sonarqube/docker/entrypoint.sh
