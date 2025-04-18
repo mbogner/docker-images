e77ae2c64a7e15bdb6d01391b78ee99d1b864786
Manuel Bogner <manuel.bogner@openresearch.com>
2025-04-18 14:21:17 +0200

    dind-alpine with healthcheck

---
+++ docker-dind-alpine-healthcheck/README.md
@@ -0,0 +1,1 @@
+Docker Dind image with healthcheck.
---
+++ docker-dind-alpine-healthcheck/docker-dind-alpine-healthcheck.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${DIR}" || exit 98
+source ../_shared.sh || exit 99
+
+# see Dockerfile for version
+build "docker-dind-alpine-healthcheck" "$PWD" "mbopm/docker-dind-alpine-healthcheck" "28.1.0-dind-alpine3.21" "dind-alpine"
