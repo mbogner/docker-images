ef022de573f9eed0b88b1d99cbecff9a17c2896e
Manuel Bogner <m@mbo.dev>
2026-04-26 23:23:44 +0200

    update versions

--- docker-dind-openjdk-alpine-jdk21/build-docker-dind-openjdk-alpine-jdk21.sh
+++ docker-dind-openjdk-alpine-jdk21/build-docker-dind-openjdk-alpine-jdk21.sh
@@ -4,5 +4,5 @@ cd "${DIR}" || exit 98
 source ../_shared.sh || exit 99
 
 # see Dockerfile for version
-docker pull mbopm/docker-dind-alpine-healthcheck:29.1.3-dind-alpine3.23
-build "docker-dind-openjdk-alpine-jdk21" "$PWD" "mbopm/docker-dind-openjdk-alpine" "29.1.3-dind-alpine3.23-jdk21"
+docker pull mbopm/docker-dind-alpine-healthcheck:29.4.1-dind-alpine3.23
+build "docker-dind-openjdk-alpine-jdk21" "$PWD" "mbopm/docker-dind-openjdk-alpine" "29.4.1-dind-alpine3.23-jdk21"
--- kafka-connect/alpine/build-kafka-connect-alpine.sh
+++ kafka-connect/alpine/build-kafka-connect-alpine.sh
@@ -8,6 +8,6 @@ VERSION="alpine_$KAFKA_SCALA_VERSION-$KAFKA_VERSION"
 $DOWNLOADS_SCRIPT "https://downloads.apache.org/kafka/$KAFKA_VERSION/kafka_$KAFKA_SCALA_VERSION-$KAFKA_VERSION.tgz" kafka.tgz || exit 97
 
 cp "$DOWNLOADS_DIR/kafka.tgz" . || exit 96
-docker pull mbopm/temurin-alpine-jre:jre21
+docker pull mbopm/openjdk-alpine-jre:jre25
 build "kafka-connect-alpine" "$DIR" "mbopm/kafka-connect" "$VERSION" "latest" || exit 1
 rm -f kafka.tgz
