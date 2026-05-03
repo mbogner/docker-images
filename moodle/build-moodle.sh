1a606254620c225b16fdbdd54fb3a133ed4c012a
Manuel Bogner <m@mbo.dev>
2026-05-03 23:57:38 +0200

    update images

--- moodle/build-moodle.sh
+++ moodle/build-moodle.sh
@@ -4,4 +4,4 @@ cd "${DIR}" || exit 99
 source ../_shared.sh || exit 98
 
 docker pull php:8.3-apache
-build "moodle" "$DIR" "mbopm/moodle" "5.1.1" "latest" || exit 1
+build "moodle" "$DIR" "mbopm/moodle" "5.2" "latest" || exit 1
--- openjdk-ubuntu-jdk21/build-openjdk-ubuntu-jdk21.sh
+++ openjdk-ubuntu-jdk21/build-openjdk-ubuntu-jdk21.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}" || exit 98
 source ../_shared.sh || exit 99
 
-docker pull ubuntu:24.04
+docker pull ubuntu:26.04
 build "openjdk-ubuntu-jdk21" "$PWD" "mbopm/openjdk-ubuntu-jdk" "jdk21"
