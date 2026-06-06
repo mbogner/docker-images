bd3dfbb32938c2d49ca78f83b8e68a0a5d03deee
Manuel Bogner <m@mbo.dev>
2026-06-07 00:52:19 +0200

    use download script

--- _compose/metabase/download-metabase.sh
+++ _compose/metabase/download-metabase.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${DIR}" || exit 98
+source ../../_shared.sh || exit 99
 
-wget https://downloads.metabase.com/latest/metabase.jar
+../../download.sh "https://downloads.metabase.com/latest/metabase.jar" "metabase.jar" || exit 99
+rm -f metabase.jar
+cp ../../downloads/metabase.jar .
